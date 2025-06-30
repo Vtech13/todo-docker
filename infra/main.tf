abd15d544f13a26f5e130eb5091597bf94888001
antoskins <antonypinel@gmail.com>
2025-06-30 15:05:22 +0200

    jpp

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 ############################################################
 # 1. Groupe de ressources Azure
 ############################################################
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "todo" {
   name     = var.resource_group_name
   location = var.location
 }
@@ -9,17 +9,17 @@ resource "azurerm_resource_group" "rg" {
 ############################################################
 # 2. Réseau (VNet, Subnets, DNS privé)
 ############################################################
-resource "azurerm_virtual_network" "todo-docker" {
-  name                = "todo-docker-vn"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_virtual_network" "todo" {
+  name                = "todo-vn"
+  location            = azurerm_resource_group.todo.location
+  resource_group_name = azurerm_resource_group.todo.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "todo-docker_psql_subnet" {
+resource "azurerm_subnet" "todo_psql_subnet" {
   name                 = "psql-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.todo-docker.name
+  resource_group_name  = azurerm_resource_group.todo.name
+  virtual_network_name = azurerm_virtual_network.todo.name
   address_prefixes     = ["10.0.2.0/24"]
 
   delegation {
@@ -33,38 +33,38 @@ resource "azurerm_subnet" "todo-docker_psql_subnet" {
   private_link_service_network_policies_enabled = true
 }
 
-resource "azurerm_subnet" "todo-docker_aca_subnet" {
+resource "azurerm_subnet" "todo_aca_subnet" {
   name                 = "aca-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.todo-docker.name
+  resource_group_name  = azurerm_resource_group.todo.name
+  virtual_network_name = azurerm_virtual_network.todo.name
   address_prefixes     = ["10.0.20.0/23"]
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
 }
 
-resource "azurerm_private_dns_zone" "todo-docker" {
-  name                = "todo-docker.postgres.database.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_private_dns_zone" "todo" {
+  name                = "todo.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.todo.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "todo-docker" {
-  name                  = "todo-dockerVnetZone.com"
-  private_dns_zone_name = azurerm_private_dns_zone.todo-docker.name
-  virtual_network_id    = azurerm_virtual_network.todo-docker.id
-  resource_group_name   = azurerm_resource_group.rg.name
-  depends_on            = [azurerm_subnet.todo-docker_psql_subnet]
+resource "azurerm_private_dns_zone_virtual_network_link" "todo" {
+  name                  = "todoVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.todo.name
+  virtual_network_id    = azurerm_virtual_network.todo.id
+  resource_group_name   = azurerm_resource_group.todo.name
+  depends_on            = [azurerm_subnet.todo_psql_subnet]
 }
 
 ############################################################
 # 3. Base de données PostgreSQL managée
 ############################################################
-resource "azurerm_postgresql_flexible_server" "todo-docker" {
-  name                          = "todo-docker-psqlflexibleserver"
-  resource_group_name           = azurerm_resource_group.rg.name
-  location                      = azurerm_resource_group.rg.location
+resource "azurerm_postgresql_flexible_server" "todo" {
+  name                          = "todo-psqlflexibleserver"
+  resource_group_name           = azurerm_resource_group.todo.name
+  location                      = azurerm_resource_group.todo.location
   version                       = "16"
-  delegated_subnet_id           = azurerm_subnet.todo-docker_psql_subnet.id
-  private_dns_zone_id           = azurerm_private_dns_zone.todo-docker.id
+  delegated_subnet_id           = azurerm_subnet.todo_psql_subnet.id
+  private_dns_zone_id           = azurerm_private_dns_zone.todo.id
   public_network_access_enabled = false
   administrator_login           = "psqladmin"
   administrator_password        = var.postgres_password
@@ -72,7 +72,7 @@ resource "azurerm_postgresql_flexible_server" "todo-docker" {
   storage_mb                    = 32768
   storage_tier                  = "P4"
   sku_name                      = "B_Standard_B1ms"
-  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.todo-docker]
+  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.todo]
 }
 
 ############################################################
@@ -80,8 +80,8 @@ resource "azurerm_postgresql_flexible_server" "todo-docker" {
 ############################################################
 resource "azurerm_storage_account" "todo-docker" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.todo.name
+  location                 = azurerm_resource_group.todo.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   public_network_access_enabled = false
@@ -94,23 +94,23 @@ resource "azurerm_storage_container" "todo-docker" {
 }
 
 ############################################################
-# 5. Environnement Container Apps Azure
+# 5. Environnement Container Apps
 ############################################################
-resource "azurerm_container_app_environment" "todo-docker_env" {
-  name                     = "aca-todo-docker-env"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  infrastructure_subnet_id = azurerm_subnet.todo-docker_aca_subnet.id
+resource "azurerm_container_app_environment" "todo_env" {
+  name                     = "aca-todo-env"
+  location                 = azurerm_resource_group.todo.location
+  resource_group_name      = azurerm_resource_group.todo.name
+  infrastructure_subnet_id = azurerm_subnet.todo_aca_subnet.id
 }
 
 ############################################################
 # 6. Applications (Containers Docker)
 ############################################################
 # 6.1 Frontend React
-resource "azurerm_container_app" "todo-docker_frontend" {
-  name                         = "todo-docker-frontend"
-  resource_group_name          = azurerm_resource_group.rg.name
-  container_app_environment_id = azurerm_container_app_environment.todo-docker_env.id
+resource "azurerm_container_app" "todo_frontend" {
+  name                         = "todo-frontend"
+  resource_group_name          = azurerm_resource_group.todo.name
+  container_app_environment_id = azurerm_container_app_environment.todo_env.id
   revision_mode                = "Single"
   max_inactive_revisions       = 0
 
@@ -129,13 +129,13 @@ resource "azurerm_container_app" "todo-docker_frontend" {
     min_replicas = 1
     max_replicas = 3
     container {
-      name   = "todo-docker-frontend"
+      name   = "todo-frontend"
       image  = var.dockerhub_frontend_image
       cpu    = 0.5
       memory = "1.0Gi"
       env {
         name  = "REACT_APP_API_URL"
-        value = "https://${azurerm_container_app.todo-docker_api.ingress[0].fqdn}"
+        value = "https://${azurerm_container_app.todo_api.ingress[0].fqdn}"
       }
       dynamic "env" {
         for_each = var.frontend_env
@@ -157,10 +157,10 @@ resource "azurerm_container_app" "todo-docker_frontend" {
 }
 
 # 6.2 Backend Express.js
-resource "azurerm_container_app" "todo-docker_api" {
-  name                         = "todo-docker-api"
-  resource_group_name          = azurerm_resource_group.rg.name
-  container_app_environment_id = azurerm_container_app_environment.todo-docker_env.id
+resource "azurerm_container_app" "todo_api" {
+  name                         = "todo-api"
+  resource_group_name          = azurerm_resource_group.todo.name
+  container_app_environment_id = azurerm_container_app_environment.todo_env.id
   revision_mode                = "Single"
   max_inactive_revisions       = 0
 
@@ -179,48 +179,36 @@ resource "azurerm_container_app" "todo-docker_api" {
     min_replicas = 1
     max_replicas = 5
     container {
-      name   = "todo-docker-api"
+      name   = "todo-api"
       image  = var.dockerhub_backend_image
       cpu    = 0.5
       memory = "1.0Gi"
       env {
         name  = "DB_HOST"
-        value = var.backend_env["DB_HOST"]
+        value = azurerm_postgresql_flexible_server.todo.fqdn
       }
       env {
         name  = "DB_USER"
-        value = var.backend_env["DB_USER"]
+        value = "psqladmin"
       }
       env {
         name  = "DB_PASSWORD"
-        value = var.backend_env["DB_PASSWORD"]
+        value = var.postgres_password
       }
       env {
         name  = "DB_NAME"
-        value = var.backend_env["DB_NAME"]
+        value = "mydatabase"
       }
       env {
         name  = "DB_PORT"
-        value = var.backend_env["DB_PORT"]
+        value = "5432"
       }
       env {
         name  = "JWT_SECRET"
         value = var.jwt_secret
       }
-      env {
-        name  = "SESSION_SECRET"
-        value = var.backend_env["SESSION_SECRET"]
-      }
-      env {
-        name  = "GOOGLE_CLIENT_ID"
-        value = var.backend_env["GOOGLE_CLIENT_ID"]
-      }
-      env {
-        name  = "GOOGLE_CLIENT_SECRET"
-        value = var.backend_env["GOOGLE_CLIENT_SECRET"]
-      }
       dynamic "env" {
-        for_each = { for k, v in var.backend_env : k => v if !contains(["DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT","SESSION_SECRET","GOOGLE_CLIENT_ID","GOOGLE_CLIENT_SECRET"], k) }
+        for_each = var.backend_env
         content {
           name  = env.key
           value = env.value
@@ -228,7 +216,6 @@ resource "azurerm_container_app" "todo-docker_api" {
       }
     }
   }
-  depends_on = [azurerm_container_app_environment.todo-docker_env]
   ingress {
     external_enabled = true
     target_port      = 3000
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,16 @@
 output "frontend_url" {
   description = "URL publique du frontend (FQDN Azure)"
-  value       = azurerm_container_app.todo-docker_frontend.latest_revision_fqdn
+  value       = azurerm_container_app.todo_frontend.ingress[0].fqdn
 }
 
 output "backend_url" {
   description = "URL publique du backend (FQDN Azure)"
-  value       = azurerm_container_app.todo-docker_api.latest_revision_fqdn
+  value       = azurerm_container_app.todo_api.ingress[0].fqdn
 }
 
 output "postgres_fqdn" {
   description = "FQDN de la base PostgreSQL Azure"
-  value       = azurerm_postgresql_flexible_server.todo-docker.fqdn
+  value       = azurerm_postgresql_flexible_server.todo.fqdn
 }
 
 output "storage_account_name" {
